060a3710b143d0b3441db6c9c641744c00faa9da
Malak235 <158207977+Malak235@users.noreply.github.com>
2025-04-26 10:45:06 -0400

    updates to homepage

--- src/components/views/HomePageView.js
+++ src/components/views/HomePageView.js
@@ -4,11 +4,26 @@ HomePageView.js
 The Views component is responsible for rendering web page with data provided by the corresponding Container component.
 It constructs a React component to display the home page.
 ================================================== */
+import './HomePageView.css';
+import { Link } from 'react-router-dom';
+
+
 const HomePageView = () => {
   // Render Home page view
   return (
-    <div >
-      <h1>Home Page</h1>
+    <div className="home-container">
+    <div className="overlay">
+      <h1>Campus Management System</h1>
+      <div className="cards-container">
+        <Link to="/campuses" className="home-card">
+          <h2>View Campuses</h2>
+        </Link>
+        <Link to="/students" className="home-card">
+          <h2>View Students</h2>
+        </Link>
+      </div>
+    </div>
+
     </div>
   );    
 }
